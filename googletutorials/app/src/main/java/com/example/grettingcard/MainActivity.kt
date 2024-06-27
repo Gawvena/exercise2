f23d48d99e40804e3e2e4f6a0ee7a29f890c47ce
Luke <118579304+Gawvena@users.noreply.github.com>
2024-06-27 18:19:15 -0500

    Update MainActivity.kt

--- googletutorials/app/src/main/java/com/example/grettingcard/MainActivity.kt
+++ googletutorials/app/src/main/java/com/example/grettingcard/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : ComponentActivity() {
             GrettingCardTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                            name = "Android",
+                            name = "Luke",
                             modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -39,7 +39,7 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Surface(color = Color.Cyan) {
         Text(
-            text = "Hello $name!",
+            text = "What's Up? $name!",
             modifier = modifier.padding(24.dp)
         )
     }
@@ -49,6 +49,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     GrettingCardTheme {
-        Greeting("Jacob")
+        Greeting("Luke")
     }
 }
